59185975c6617383cace91d376a2fbc7043379e9
WBrandes <brandesw@moravian.edu>
2019-05-30 11:37:17 -0400

    Add multi-grade functionality

--- test_grades.py
+++ test_grades.py
@@ -1,6 +1,15 @@
 
 from grades import compute_hw_average
 
+'''
+                    |
+____________    __ -+-  ____________
+\_____     /   /_ \ |   \     _____/
+ \_____    \____/  \____/    _____/
+  \_____                    _____/
+     \___________  ___________/
+               /____\
+'''
 
 def test_zero_grades():
     grades = []
@@ -10,3 +19,7 @@ def test_zero_grades():
 def test_single_grade():
     grades = [42]
     assert compute_hw_average(grades) == 42
+
+def test_many_grades():
+    grades = [30, 15, 15]
+    assert compute_hw_average(grades) == 20
